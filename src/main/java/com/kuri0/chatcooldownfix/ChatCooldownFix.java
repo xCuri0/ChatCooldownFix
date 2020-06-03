7ec5ae10eb50d3ebc63fabef1fdc7bb7c689e592
Curi0 <zkqri0@gmail.com>
2020-06-03 16:31:50 +0530

    formatting, fixed delay and dont delay forge commands

--- src/main/java/com/kuri0/chatcooldownfix/ChatCooldownFix.java
+++ src/main/java/com/kuri0/chatcooldownfix/ChatCooldownFix.java
@@ -1,4 +1,5 @@
 package com.kuri0.chatcooldownfix;
+
 import java.util.ArrayList;
 
 import net.minecraft.client.Minecraft;
@@ -13,64 +14,61 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 @Mod(modid = ChatCooldownFix.MODID, version = ChatCooldownFix.VERSION)
-public class ChatCooldownFix
-{
-    public static final String MODID = "ChatCooldownFix";
-    public static final String VERSION = "1.0";
+public class ChatCooldownFix {
+	public static final String MODID = "ChatCooldownFix";
+	public static final String VERSION = "1.0";
 
-    public static ArrayList<String> queue = new ArrayList<String>();
+	public static ArrayList<String> queue = new ArrayList<String>();
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-    	MinecraftForge.EVENT_BUS.register(this);
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(this);
 
-    	// FIFO chat message queue thread
+		// FIFO chat message queue thread
 		(new Thread() {
 			public void run() {
 				while (true) {
 					if (queue.size() == 0) {
-						synchronized(com.kuri0.chatcooldownfix.ChatCooldownFix.queue) {
+						synchronized (com.kuri0.chatcooldownfix.ChatCooldownFix.queue) {
 							try {
 								com.kuri0.chatcooldownfix.ChatCooldownFix.queue.wait();
 							} catch (InterruptedException e) { }
 						}
 					}
 
-					// from GuiScreen
 					Minecraft.getMinecraft().ingameGUI.getChatGUI().addToSentMessages(queue.get(0));
-			        if (net.minecraftforge.client.ClientCommandHandler.instance.executeCommand(Minecraft.getMinecraft().thePlayer, queue.get(0)) == 0)
-			        	Minecraft.getMinecraft().thePlayer.sendChatMessage(queue.get(0));
+					Minecraft.getMinecraft().thePlayer.sendChatMessage(queue.get(0));
 
 					queue.remove(0);
 					try {
-							Thread.sleep(3500);
+						Thread.sleep(3100);
 					} catch (InterruptedException e) { }
 				}
 			}
 		}).start();
-    }
-    
-    
-    // replace GUIChat with our own that has a modified sendChatMessage
-    @SubscribeEvent
-    public void onGuiOpen(GuiOpenEvent event) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-    	String name = "";
-    	try {
-    		 name = event.gui.getClass().getName();
-    	} catch (NullPointerException e) {
-    		return;
-    	}
-    	
-    	if (name == "net.minecraft.client.gui.GuiChat") {
-    		String defaultInputFieldText = ReflectionHelper.getPrivateValue(GuiChat.class, (GuiChat)event.gui, "defaultInputFieldText", "field_146409_v");
-    		event.gui = new MyGuiChat(defaultInputFieldText);
-    	}
-   }
-    
-   // needed so we dont get nullpointerexception after disconnecting
-   @SubscribeEvent
-   public void onPlayerLoggedOut(PlayerEvent.PlayerLoggedOutEvent event) {
-	   	queue.clear();
-   }
+	}
+
+	// replace GUIChat with our own that has a modified sendChatMessage
+	@SubscribeEvent
+	public void onGuiOpen(GuiOpenEvent event)
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		String name = "";
+		try {
+			name = event.gui.getClass().getName();
+		} catch (NullPointerException e) {
+			return;
+		}
+
+		if (name == "net.minecraft.client.gui.GuiChat") {
+			String defaultInputFieldText = ReflectionHelper.getPrivateValue(GuiChat.class, (GuiChat) event.gui,
+					"defaultInputFieldText", "field_146409_v");
+			event.gui = new MyGuiChat(defaultInputFieldText);
+		}
+	}
+
+	// needed so we dont get nullpointerexception after disconnecting
+	@SubscribeEvent
+	public void onPlayerLoggedOut(PlayerEvent.PlayerLoggedOutEvent event) {
+		queue.clear();
+	}
 }
--- src/main/java/com/kuri0/chatcooldownfix/MyGuiChat.java
+++ src/main/java/com/kuri0/chatcooldownfix/MyGuiChat.java
@@ -1,19 +1,25 @@
 package com.kuri0.chatcooldownfix;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class MyGuiChat extends GuiChat {
 
-    public MyGuiChat(String defaultText) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
-    {
-    	ReflectionHelper.setPrivateValue(GuiChat.class, this, defaultText, "defaultInputFieldText", "field_146409_v");
-    }
+	public MyGuiChat(String defaultText)
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		ReflectionHelper.setPrivateValue(GuiChat.class, this, defaultText, "defaultInputFieldText", "field_146409_v");
+	}
 
 	@Override
-	public void sendChatMessage (String msg) {
+	public void sendChatMessage(String msg) {
+		if (net.minecraftforge.client.ClientCommandHandler.instance.executeCommand(Minecraft.getMinecraft().thePlayer,
+				msg) != 0) {
+			Minecraft.getMinecraft().ingameGUI.getChatGUI().addToSentMessages(msg);
+			return;
+		}
 		com.kuri0.chatcooldownfix.ChatCooldownFix.queue.add(msg);
-		synchronized(com.kuri0.chatcooldownfix.ChatCooldownFix.queue) {
+		synchronized (com.kuri0.chatcooldownfix.ChatCooldownFix.queue) {
 			com.kuri0.chatcooldownfix.ChatCooldownFix.queue.notify();
 		}
 	}
